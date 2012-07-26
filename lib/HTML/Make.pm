bb84489b3d69ec0f0549da5cd15041b01c3bb080
Ben Bullock <benkasminbullock@gmail.com>
2012-07-27 08:31:06 +0900

    Initial commit of HTML-Make

---
+++ lib/HTML/Make.pm
@@ -0,0 +1,13 @@
+=head1 NAME
+
+HTML::Make
+
+=cut
+package HTML::Make;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
---
+++ t/HTML-Make.t
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+use Test::More tests => 1;
+BEGIN { use_ok('HTML::Make') };
+use HTML::Make;
+
+# Local variables:
+# mode: perl
+# End:
